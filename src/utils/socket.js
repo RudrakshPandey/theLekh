6161d712001e59230c77ea81bffb5614c3b5b296
RudrakshPandey <rudrakshpandey8@gmail.com>
2025-06-08 17:48:06 +0530

    checking

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+console.log("Socket URL:", import.meta.env.VITE_SOCKET_URL);
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false, // Prevent auto-connecting; call socket.connect() manually
