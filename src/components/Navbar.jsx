b51ac5c28a8193b3282896b0486085203928b0de
RudrakshPandey <rudrakshpandey8@gmail.com>
2025-06-08 18:03:04 +0530

    fixed navbar of landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import { useContext, useEffect, useRef, useState } from 'react';
-import { Link, useNavigate, NavLink } from 'react-router-dom';
-import AuthContext from '../context/AuthContext';
-import { MdOutlineMenuOpen, MdPostAdd } from 'react-icons/md';
-import { RxAvatar, RxDashboard } from 'react-icons/rx';
-import { RiLogoutBoxRLine } from 'react-icons/ri';
-import { MdAddCircleOutline } from 'react-icons/md';
-import '../styles/navbar.css';
-import Logo from './Logo';
+import axios from "axios";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Link, useNavigate, NavLink } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import { MdOutlineMenuOpen, MdPostAdd } from "react-icons/md";
+import { RxAvatar, RxDashboard } from "react-icons/rx";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { MdAddCircleOutline } from "react-icons/md";
+import "../styles/navbar.css";
+import Logo from "./Logo";
 const react_base_url = import.meta.env.VITE_API_BASE_URL;
 const Navbar = () => {
   const { logout, user } = useContext(AuthContext);
@@ -16,12 +16,13 @@ const Navbar = () => {
   const navigate = useNavigate();
   const dropdownRef = useRef(null);
   const dropdownRefMb = useRef(null);
+  const justToggledMbRef = useRef(false);
 
   const handleSignout = async (e) => {
     e.preventDefault();
     try {
       logout();
-      navigate('/');
+      navigate("/");
       await axios.post(`${react_base_url}/users/logout`);
       // console.log(response.data);
     } catch (error) {
@@ -33,96 +34,106 @@ const Navbar = () => {
     setShowDropdown(!showDropdown);
   };
   const toggleDropdownMb = () => {
-    console.log('mobile click');
-    setShowDropdownMb(!showDropdownMb);
+    setShowDropdownMb((prev) => {
+      const newVal = !prev;
+      justToggledMbRef.current = true;
+      return newVal;
+    });
   };
 
   // listen for outside div clicks
   useEffect(() => {
     const handleClickOutside = (e) => {
+      if (justToggledMbRef.current) {
+        justToggledMbRef.current = false;
+        return; // Skip this click
+      }
+
       if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
         setShowDropdown(false);
       }
+
       if (dropdownRefMb.current && !dropdownRefMb.current.contains(e.target)) {
         setShowDropdownMb(false);
       }
     };
-    document.addEventListener('mousedown', handleClickOutside);
+
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
   return (
-    <div className='navbar-0'>
-      <nav className='navbar'>
-        <div className='navbar-brand'>
-          <Link to='/'>
+    <div className="navbar-0">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/">
             <Logo />
           </Link>
         </div>
         {!user ? (
-          <div className='navbar-right'>
-            <div className='navbar-user-action-c'>
-              <Link to='/login' className='navbar-user-action-btn'>
+          <div className="navbar-right">
+            <div className="navbar-user-action-c">
+              <Link to="/login" className="navbar-user-action-btn">
                 login
               </Link>
-              <Link to='/register' className='navbar-user-action-btn'>
+              <Link to="/register" className="navbar-user-action-btn">
                 sign up
               </Link>
             </div>
 
             <MdOutlineMenuOpen
-              className='navbar-menu-icon'
+              className="navbar-menu-icon"
               onClick={toggleDropdownMb}
             />
             {showDropdownMb && (
-              <div className='navbar-dropdown-mb' ref={dropdownRefMb}>
+              <div className="navbar-dropdown-mb" ref={dropdownRefMb}>
                 <ul>
                   <li>
-                    <Link to='/login'>login</Link>
+                    <Link to="/login">login</Link>
                   </li>
                   <li>
-                    <Link to='/register'>sign up</Link>
+                    <Link to="/register">sign up</Link>
                   </li>
                 </ul>
               </div>
             )}
           </div>
         ) : (
-          <div className='navbar-right'>
-            <Link to='/blogs/blog/create-blog' className='navbar-add-blog-btn'>
+          <div className="navbar-right">
+            <Link to="/blogs/blog/create-blog" className="navbar-add-blog-btn">
               <MdAddCircleOutline /> Add blog
             </Link>
-            <div className='navbar-avatar' onClick={toggleDropdown}>
+            <div className="navbar-avatar" onClick={toggleDropdown}>
               {user?.avatar_url ? (
                 <img
                   src={user.avatar_url}
-                  alt='navbar-avatar'
-                  className='navbar-avatar-img'
+                  alt="navbar-avatar"
+                  className="navbar-avatar-img"
                 />
               ) : (
-                <div className='navbar-avatar-initials'>
+                <div className="navbar-avatar-initials">
                   {user?.name
-                    ?.split(' ')
+                    ?.split(" ")
                     ?.map((n) => n[0])
-                    ?.join('')}
+                    ?.join("")}
                 </div>
               )}
               <MdOutlineMenuOpen
-                className='navbar-menu-icon'
+                className="navbar-menu-icon"
                 onClick={toggleDropdown}
               />
               {showDropdown && (
-                <div className='navbar-dropdown' ref={dropdownRef}>
+                <div className="navbar-dropdown" ref={dropdownRef}>
                   <p>Signed in as: {user.email}</p>
                   <ul>
-                    <li className='only-mb-d'>
+                    <li className="only-mb-d">
                       <NavLink
-                        to='/blogs/blog/create-blog'
+                        to="/blogs/blog/create-blog"
                         className={({ isActive }) =>
                           isActive
-                            ? 'navbar-navlinks-active'
-                            : 'navbar-navlinks'
+                            ? "navbar-navlinks-active"
+                            : "navbar-navlinks"
                         }
                       >
                         <MdPostAdd /> Add Blog
@@ -130,11 +141,11 @@ const Navbar = () => {
                     </li>
                     <li>
                       <NavLink
-                        to='/profile'
+                        to="/profile"
                         className={({ isActive }) =>
                           isActive
-                            ? 'navbar-navlinks-active'
-                            : 'navbar-navlinks'
+                            ? "navbar-navlinks-active"
+                            : "navbar-navlinks"
                         }
                       >
                         <RxAvatar /> profile
@@ -142,11 +153,11 @@ const Navbar = () => {
                     </li>
                     <li>
                       <NavLink
-                        to='/dashboard'
+                        to="/dashboard"
                         className={({ isActive }) =>
                           isActive
-                            ? 'navbar-navlinks-active'
-                            : 'navbar-navlinks'
+                            ? "navbar-navlinks-active"
+                            : "navbar-navlinks"
                         }
                       >
                         <RxDashboard /> dashboard
