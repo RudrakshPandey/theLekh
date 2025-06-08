6068c7fd29066f5ad6dea6b2ce4c1fd49506fb02
RudrakshPandey <rudrakshpandey8@gmail.com>
2025-06-08 17:19:51 +0530

    fixed prod url for backend socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-// const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || ";
+const SOCKET_URL =
+  process.env.REACT_APP_SOCKET_URL || "http://localhost:5000";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io(SOCKET_URL, {
   autoConnect: false, // Prevent auto-connecting; call socket.connect() manually
 });
